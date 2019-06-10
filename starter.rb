b89162dccfce8845863388529bac1c0fb2e5b23a
T-Factorial <[taylorbleizeffer@gmail.com]>
2019-06-10 14:48:58 -0400

    Laid groundwork for scraping TOSEM. Dug to articles. ToDo: figure out how to get article links under Table of Contents.

--- starter.rb
+++ starter.rb
@@ -7,11 +7,21 @@ def parseArguments()
     else
         helpers = Helpers.new
         url = ARGV[0]
+
+        # Set up agent ang get page specified in ARGV[0]
         agent = helpers.getAgent()
+        agent.user_agent_alias = "Linux Mozilla"
         page = helpers.getPage(url,agent)
-        page.links.each do |link|
-            print link
-            print "\n"
+
+        # Switch to single page view
+        singleview_page = page.link_with(:text => 'single page view').click
+
+        # Get all links to Volumes published after 2008
+        volumes = singleview_page.links_with(:text => %r{(2009|201[0-9])})
+        volumes = volumes.drop(4)
+
+        volumes.each do |vol|
+            vol_page = vol.click.link_with(:text => 'table of contents').click
         end
     end
 end
