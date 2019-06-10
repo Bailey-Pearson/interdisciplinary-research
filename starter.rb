031d755a109454dbdc9254997a5edd96184a92db
T-Factorial <[taylorbleizeffer@gmail.com]>
2019-06-10 16:31:05 -0400

    Got down to Table of Content links. Now need to filter so only Article links remain.

--- starter.rb
+++ starter.rb
@@ -10,7 +10,7 @@ def parseArguments()
 
         # Set up agent ang get page specified in ARGV[0]
         agent = helpers.getAgent()
-        agent.user_agent_alias = "Linux Mozilla"
+        #agent.user_agent_alias = "Linux Mozilla"
         page = helpers.getPage(url,agent)
 
         # Switch to single page view
@@ -20,8 +20,17 @@ def parseArguments()
         volumes = singleview_page.links_with(:text => %r{(2009|201[0-9])})
         volumes = volumes.drop(4)
 
+        # Follow Volume links and navigate to corresponding Table of Contents
         volumes.each do |vol|
-            vol_page = vol.click.link_with(:text => 'table of contents').click
+            vol_page = vol.click
+            print vol_toc = vol_page.links_with(:href => %r{citations}).count
+
+            /vol_toc = vol_page.links.find_all{|l| l.attributes.parent.name == "span" and l.attributes.parent.parent.name == "td"}
+
+            vol_toc.links_with(:href => 'citations')
+            vol_toc.each do |link|
+                puts link
+            end/
         end
     end
 end
