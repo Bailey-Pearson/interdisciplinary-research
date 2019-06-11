1813123b9e872f9fbbdbd53895c29077ab8ed8d1
amlopa16 <alopata98@gmail.com>
2019-06-11 14:48:11 -0400

    checkpoint

--- link1.rb
+++ link1.rb
@@ -4,7 +4,7 @@ require "mechanize"
 require_relative "helpers"
 require 'string_pattern'
 
-def parseArguments()
+def getLinks()
     if ARGV.length < 1
         print "Not Enough Arguments\n"
     else
@@ -12,26 +12,34 @@ def parseArguments()
         url = ARGV[0]
         agent = helpers.getAgent()
         page = helpers.getPage(url,agent)
-        #volumes = page.links_with(:text => %r{(2009|201[0-9])})
+        links_correct_years = page.links_with(:text => %r{(2009|201[0-9])})
         icse_regex1 = Regexp.compile(%r{.*[\s;:,-/]+ICSE[\s;:,-/]+.*})
         icse_regex2 = Regexp.compile(%r{^ICSE[\s;:',-/]+.*})
         icse_regex3 = Regexp.compile(%r{.*ICSE$})
         ase_regex = Regexp.compile(%r{^ASE[\s;:',-/]+.*})
         fse_regex = Regexp.compile(%r{.*[\s;:,-/]+FSE[\s;:,-/]+.*})
-        page.links.each do |link|
+
+        links = []
+        links_correct_years.each do |link|
             if fse_regex.match?(link.text) | ase_regex.match?(link.text) | icse_regex1.match?(link.text) | icse_regex2.match?(link.text) | icse_regex3.match?(link.text)
-                #new_page = link.click
+                links << link
                 print link
                 print "\n"
             end
         end
     end
+    #return links
+end
+
+def getArticles()
+
+    articles = []
+    page_links = getLinks()
+    page_links.each do |page_link|
+        print page_link
+        print "\n"
+    end
 end
 
 
-# [ link.match(icse), "ASE ", "FSE "].any? {|str| link.text.include? str} && !["CHASE", "CASES", "EASE", "ICSEB", "ICSET", "ITiCSE", "TEFSE"].any? {|str2| link.text.include? str2}
-
-parseArguments()
-
-
-# ase_regex.match?(link.text) | fse_regex.match?(link.text) | icse_regex.match?(link.text)
+getLinks()
