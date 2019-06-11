bbd93730a8121be81bcf3ea7ec449abbb1b9502d
T-Factorial <[taylorbleizeffer@gmail.com]>
2019-06-11 13:12:10 -0400

    Split getArticles into two methods: getVolumes and getArticles.

--- starter.rb
+++ starter.rb
@@ -3,14 +3,14 @@ require_relative "helpers"
 require_relative "article"
 require_relative "author"
 
-def getArticles()
+def getVolumes()
     if ARGV.length < 1
         print "Not Enough Arguments\n"
     else
         helpers = Helpers.new
         url = ARGV[0]
 
-        # Set up agent ang get page specified in ARGV[0]
+        # Set up agent and get page specified in ARGV[0]
         agent = helpers.getAgent()
         #agent.user_agent_alias = "Linux Mozilla"
         page = helpers.getPage(url,agent)
@@ -24,30 +24,32 @@ def getArticles()
         # This line may need reworked, but it works for now.
         volumes = volumes.drop(4)
 
-        # Array to hold article links
-        articles = []
-
-        # Follow Volume links and navigate to corresponding Table of Contents
-        volumes.each do |vol|
-
-            # Visit volume
-            vol_page = vol.click
-
-            # Grab all Article links
-            vol_toc = vol_page.links_with(:href => %r{citation\.cfm\?id=[0-9]*})
-            vol_toc = vol_toc.select {|link| link.attributes.values.count == 1 and link.text != 'tabbed view' and !link.text.include? "Editorial"}
-
-            # Place each article link into the article array
-            vol_toc.each do |link|
-                articles << link
-            end
-        end
-
-        # Return the array of article links
-        return articles
+        return volumes
     end
 end
 
-getArticles().each do |link|
-  puts link
+def getArticles(volumes)
+
+    # Array to hold article links
+    articles = []
+
+    # Follow Volume links and navigate to corresponding Table of Contents
+    volumes.each do |vol|
+
+        # Visit volume
+        vol_page = vol.click
+
+        # Grab all Article links
+        vol_toc = vol_page.links_with(:href => %r{citation\.cfm\?id=[0-9]*})
+        vol_toc = vol_toc.select {|link| link.attributes.values.count == 1 and link.text != 'tabbed view' and !link.text.include? "Editorial"}
+
+        # Place each article link into the article array
+        vol_toc.each do |link|
+            articles << link
+        end
+    end
+
+    # Return the array of article links
+    return articles
+
 end
