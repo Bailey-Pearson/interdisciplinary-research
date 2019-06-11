9d7cbb17da97a5387d5937b611b6651bd1355107
amlopa16 <alopata98@gmail.com>
2019-06-11 11:14:24 -0400

    got all the links

--- link1.rb
+++ link1.rb
@@ -1,4 +1,4 @@
-# Amber
+# Amber Lopata
 
 require "mechanize"
 require_relative "helpers"
@@ -13,11 +13,13 @@ def parseArguments()
         agent = helpers.getAgent()
         page = helpers.getPage(url,agent)
         #volumes = page.links_with(:text => %r{(2009|201[0-9])})
-        #icse_regex = /^[^a-zA-Z]*$["ICSE "]/
-        #ase_regex = /^[^a-zA-Z]*$["ASE "]/
-        #fse_regex = /^[^a-zA-Z]*$["FSE "]/
+        icse_regex1 = Regexp.compile(%r{.*[\s;:,-/]+ICSE[\s;:,-/]+.*})
+        icse_regex2 = Regexp.compile(%r{^ICSE[\s;:',-/]+.*})
+        icse_regex3 = Regexp.compile(%r{.*ICSE$})
+        ase_regex = Regexp.compile(%r{^ASE[\s;:',-/]+.*})
+        fse_regex = Regexp.compile(%r{.*[\s;:,-/]+FSE[\s;:,-/]+.*})
         page.links.each do |link|
-            if link.text.include?("ICSE ") | link.text.include?("ASE ") | link.text.include?("FSE ")
+            if fse_regex.match?(link.text) | ase_regex.match?(link.text) | icse_regex1.match?(link.text) | icse_regex2.match?(link.text) | icse_regex3.match?(link.text)
                 #new_page = link.click
                 print link
                 print "\n"
@@ -30,3 +32,6 @@ end
 # [ link.match(icse), "ASE ", "FSE "].any? {|str| link.text.include? str} && !["CHASE", "CASES", "EASE", "ICSEB", "ICSET", "ITiCSE", "TEFSE"].any? {|str2| link.text.include? str2}
 
 parseArguments()
+
+
+# ase_regex.match?(link.text) | fse_regex.match?(link.text) | icse_regex.match?(link.text)
